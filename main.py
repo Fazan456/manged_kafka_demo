118da7251a4906c86dc4fd03f0788e2bae433b70
Mohamed Fazan <your.email@example.com>
2024-12-19 13:22:33 +0530

    kafka_producer

--- main.py
+++ main.py
@@ -1,15 +1,39 @@
 from confluent_kafka import Producer
 from confluent_kafka.admin import AdminClient, NewTopic
+import logging
+import random
+import uuid
+import json
+import time
+
 
 KAFKA_BROKERS = "localhost:29092,localhost:39092,localhost:49092"
+NUM_PARTITIONS = 4
+REPLICATION_FACTOR = 3
+TOPIC_NAME = "financial_transactions"
+logging.basicConfig(
+level=logging.INFO
+)
+
+loggers = logging.getLogger(__name__)
+
+producer_conf = {
+    'bootstrap.servers': KAFKA_BROKERS,
+    'queue.buffering.max.messages': 100000,
+    'queue.buffering.max.kbytes':512000,
+    'batch.num.messages':1000,
+    'linger.ms':10,
+    'acks':1,
+    'compression.type':'gzip'
+
+}
+producer = Producer(producer_conf)
 
 def create_topic(topic_name):
     """
     Create a Kafka topic with the given name.
     """
     admin_client = AdminClient({"bootstrap.servers": KAFKA_BROKERS})
-    NUM_PARTITIONS = 4
-    REPLICATION_FACTOR = 3
 
     try:
         metadata = admin_client.list_topics(timeout=10)
@@ -24,16 +48,67 @@ def create_topic(topic_name):
             for topic, future in fs.items():
                 try:
                     future.result()  
-                    print(f"Topic '{topic_name}' created successfully.")
+                    loggers.info(f"Topic '{topic_name}' created successfully.")
                 except Exception as e:
-                    print(f"Failed to create topic '{topic_name}': {e}")
+                    loggers.error(f"Failed to create topic '{topic_name}': {e}")
         else:
-            print(f"Topic '{topic_name}' already exists.")
+            loggers.info(f"Topic '{topic_name}' already exists.")
 
     except Exception as e:
-        print(f"Error creating topic: {e}")
+        loggers.error(f"Error creating topic: {e}")
+
+
+def generate_transaction():
+    return dict(
+        transactionId = str(uuid.uuid4()),
+        UserId = f"user_{random.randint(1,100)}",
+        amount = round(random.uniform(50000,150000),2),
+        transaction_time = int(time.time()),
+        merchantId = random.choice(['merchant1','merchant2','merchant3']),
+        transactionType = random.choice(['purchase','refund']),
+        location = f'location_{random.randint(1,50)}',
+        paymentMethod = random.choice(['credit_card','paypal','bank_transfer']),
+        inInernational = random.choice(['True','False']),
+        currency = random.choice(['USD','LKR'])
+    )
+
+def delivery_report(err,msg):
+    if err is not None:
+        print(f'Delivery failed for record {msg.key()}')
+    else:
+        print(f'Record {msg.key()} successfully produced')   
+
+def produce_transaction():
+    while True:
+        transaction = generate_transaction()
+        try:
+            producer.produce(
+            topic = TOPIC_NAME,
+            key = transaction['UserId'],
+            value = json.dumps(transaction).encode('utf-8'),
+            on_delivery = delivery_report
+                )
+            print(f'Produced Transaction: {transaction}')
+            producer.flush()
+        except Exception as e:
+            print(f'Error sending transaction: {e}')
+
+
+def produce_data_in_parallel(num_threads):
+    threads  = []
+    try:
+        for i in range(num_threads):
+            thread = threading.Thread(target = produce_transaction, args=(i,))
+            thread.deamon = True
+            thread.start()
+            threads.append(thread)
+            for thread in threads:
+                thread.join()
+    except Exception as e:
+        print('Error Message {e}')
+
 
 
 if __name__ == "__main__":
-    TOPIC_NAME = "my_test_topic"  
     create_topic(TOPIC_NAME)
+    produce_data_in_parallel(3)
